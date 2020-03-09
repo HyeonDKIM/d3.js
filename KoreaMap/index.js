c8c9210eb7b506429c9237ed68e74e6ee4b8a987
HyeonDKIM <aquaifnt7@gmail.com>
2020-03-09 17:48:18 +0900

    Update

--- KoreaMap/index.js
+++ KoreaMap/index.js
@@ -1,11 +1,17 @@
 var FILE_DEFAULT = "main.html";
-
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
 const fs = require('fs')
-const port = 8090;
+var port = 8090;
 
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res)=>{
   fs.readFile(FILE_DEFAULT, function(error, data){
@@ -15,3 +21,42 @@ app.get('/', (req, res)=>{
 });
 
 app.listen(port, () => console.log("server opened port 8090"));
+
+mongoose.connect('mongodb://localhost:27017/map'); // 기본 설정에 따라 포트가 상이 할 수 있습니다.
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("mongo db connection OK.");
+});
+
+
+
+/*
+var testSchema = mongoose.Schema({
+	name: String
+});
+
+testSchema.methods.speak = function () {
+	var greeting = this.name
+	? "Meow name is " + this.name
+	: "I don't have a name"
+	console.log("speak() - " + greeting);
+}
+
+var TestModel = mongoose.model("TestModel", testSchema);
+
+var testIns = new TestModel({ name: "testIns"});
+
+testIns.save(function(err, testIns){
+	if(err) return console.error(err);
+	testIns.speak();
+});
+
+TestModel.find(function(err, models){
+	if(err) return console.error(err);
+	console.log("find() - "+models);
+});
+
+TestModel.find({name:/^testIns/});
+
+*/
