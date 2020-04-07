a51cdb2f28ac38e8ee0dc4964b2d7613f9b19db3
HyeonDKIM <aquaifnt7@gmail.com>
2020-04-07 23:01:26 +0900

    디비나 만들자...

--- KoreaMap/option.js
+++ KoreaMap/option.js
@@ -1,26 +1,36 @@
+/*
+references
+https://velog.io/@dooreplay/%EC%9C%84%EC%BD%94%EB%93%9C-5-6%EC%9D%BC%EC%B0%A8-TIL-js-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%ED%95%A8%EC%88%98%EC%99%80-json-%EB%8D%B0%EC%9D%B4%ED%84%B0-7gjywv7yz4
+
+*/
+var tmp_name = [];
+
 function categoryChange(e) {
+  var json_gu = [];
+  var json_topo ='./topology/'+String(e.value)+'-topo.json';
+  fetch(json_topo) // json파일이 있는 파일의 경로
+    .then((res) => res.json()) // 데이터를 json형태로 바꿔주기
+    .then(function (data) { // data라는 임의의 값을 인자로 받는 함수.
 
-   // var json_topo = require('./topology/'+e.value+'-topo.json');
+      //여기 수정 요망!!!!!
+      json_gu = data.objects.Seoul.geometries; //json파일을 감싸는 객체가 result로 시작해서 data.result
+      for(var i=0; i<json_gu.length; i++){
+        var json_gu_tmp = json_gu[i].properties.SIG_ENG_NM;
+        tmp_name.push(json_gu_tmp);
+        console.log(json_gu_tmp);
+      }
+      updateName(e);
+    });
+  }
 
-  //  var json_gu_length = json_topo.properties.SIG_ENG_NM.length;
-
-    //console.log(json_gu_length);
-
+function updateName(e) {
 //예제
-    var good_a = ["ㅁ", "ㄴ", "ㅇ", "ㄹ"];
-    var good_b = ["ㅂ", "ㅈ", "ㄷ", "ㄱ"];
-    var good_c = ["ㅋ", "ㅌ", "ㅊ", "ㅍ"];
     var target = document.getElementById("sido_name");
-   
     console.log(e.value);
 
-    if(e.value == "Seoul") {
-        var d = good_a;
-    }
-    else if(e.value == "b") var d = good_b;
-    else if(e.value == "c") var d = good_c;
-   
-   target.options.length = 0;
+    var d = tmp_name;
+    console.log(d);
+    target.options.length = 0;
    
     for (x in d) {
       var opt = document.createElement("option");
